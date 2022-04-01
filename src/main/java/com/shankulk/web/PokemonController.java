9f094a951e0408993d42a03cb617caec62d7d75c
Shantanu Kulkarni <shankulk@gmail.com>
2022-04-02 00:59:14 +0100

    MVC test
    Add Web layer test for first API endpoint

--- src/main/java/com/shankulk/web/PokemonController.java
+++ src/main/java/com/shankulk/web/PokemonController.java
@@ -5,14 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/pokemon")
 public class PokemonController {
 
-    @GetMapping("/{name}")
-    public ResponseEntity<PokemonResponse> getSimplePokemon(@PathVariable("name") final String pokemonName) {
+    @GetMapping(value = "/{name}", produces = APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<PokemonResponse> getSimplePokemon(@PathVariable("name") final String pokemonName) {
         return ResponseEntity.ok(new PokemonResponse(pokemonName, "This is sample Pokemon", "Earth", false));
     }
 }
---
+++ src/test/java/com/shankulk/web/PokemonControllerWebMvcTest.java
@@ -0,0 +1,30 @@
+package com.shankulk.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class PokemonControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Verify Get Simple Pokemon returns a success response")
+    void getSimplePokemon() throws Exception {
+        MvcResult mvcResult = mvc
+                .perform(MockMvcRequestBuilders.get("/pokemon/misty").header("accept", "application/json"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("misty");
+    }
+}
