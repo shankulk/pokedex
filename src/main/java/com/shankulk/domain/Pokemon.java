f45be80b39b3a4761a015821618cfeec2c83623e
Shantanu Kulkarni <shankulk@gmail.com>
2022-04-03 15:59:07 +0100

    Simplify getDescription

--- src/main/java/com/shankulk/domain/Pokemon.java
+++ src/main/java/com/shankulk/domain/Pokemon.java
@@ -1,11 +1,13 @@
 package com.shankulk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
-import java.util.Optional;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -18,10 +20,18 @@ public class Pokemon {
 
     private String habitat;
 
+    @JsonIgnore
+    private String description;
+
+    @Getter(AccessLevel.NONE)
     @JsonProperty("flavor_text_entries")
     private List<Description> descriptions;
 
-    public Optional<Description> getEnglishDescription() {
-        return descriptions.stream().filter(description -> "en".equalsIgnoreCase(description.getLanguage().getName())).findAny();
+    public String getDescription() {
+        return descriptions
+                .stream()
+                .filter(description -> "en".equalsIgnoreCase(description.getLanguage().getName()))
+                .findAny().get()
+                .getDescription();
     }
 }
